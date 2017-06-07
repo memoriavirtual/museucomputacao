bc78dacaf6065829c7744ca7b68fd6a1e057cd72
Rogiel <rogiel2009@gmail.com>
2017-06-06 22:56:32 -0300

    arrumado o nav e slides

--- siteMuseu/javascript/funcoes.js
+++ siteMuseu/javascript/funcoes.js
@@ -1,49 +1,14 @@
-/**
- * @author rogiel
- */
+var quantImg = 19;
+var i = 0;
+// quantidade de imagem que deseja exibir;
 
-function slidePrincipal1() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00660.jpg";
-	setTimeout("slidePrincipal2()", 2000);
-}
+function slidePrincipal() {
 
-function slidePrincipal2() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00661.jpg";
-	setTimeout("slidePrincipal3()", 2000);
-}
-
-function slidePrincipal3() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00662.jpg";
-	setTimeout("slidePrincipal4()", 2000);
-}
-function slidePrincipal4(){
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00663.jpg";
-	setTimeout("slidePrincipal5()", 2000);
-}
-function slidePrincipal5(){
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00664.jpg";
-	setTimeout("slidePrincipal6()", 2000);
-}
-function slidePrincipal6() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00665.jpg";
-	setTimeout("slidePrincipal7()", 2000);
-}
-
-function slidePrincipal7() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00666.jpg";
-	setTimeout("slidePrincipal8()", 2000);
-}
-function slidePrincipal8() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00667.jpg";
-	setTimeout("slidePrincipal9()", 2000);
-}
-function slidePrincipal9() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00668.jpg";
-	setTimeout("slidePrincipal10()", 2000);
-}
-function slidePrincipal10() {
-	document.getElementById('id').src = "images/ImagensSlides/slideIMG_00669.jpg";
-	setTimeout("slidePrincipal1()", 2000);
-}
+	document.getElementById('id').src = "images/ImagensSlides/" + i + ".jpg";
+	setTimeout("slidePrincipal()", 2500);
+	if (i >= quantImg)
+		i = -1;
+	i++;
 
 
+}
--- siteMuseu/javascript/singlePage.js
+++ siteMuseu/javascript/singlePage.js
@@ -1,60 +1,114 @@
+var idAnterior = "000"// nenhum id no inicio
+
 $(document).ready(function() {
 
 	$(document).on("click", "a#oMuseu", function() {
-		$("section#centralSection").load("museu/o_museu.html");
+		$(idAnterior).addClass("voltaMenuBranco");
+		$("section#centralSection").load("museu/oMuseu.html");
+		$("#mainMuseu").removeClass("voltaMenuBranco");
+		$("#mainMuseu").addClass("active");
+		idAnterior = "#mainMuseu";
+
+	});
+	$(document).on("click", "a#museuMidia", function() {
+
+		$(idAnterior).addClass("voltaMenuBranco");
+		$("section#centralSection").load(" ");
+		$("#mainMuseu").removeClass("voltaMenuBranco");
+		$("#mainMuseu").addClass("active");
+		idAnterior = "#mainMuseu";
+
 	});
 	$(document).on("click", "a#oFundador", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("museu/o-fundador.html");
+		$("#mainMuseu").removeClass("voltaMenuBranco");
+		$("#mainMuseu").addClass("active");
+		idAnterior = "#mainMuseu";
+
 	});
 
 	/*institucional*/
-	$(document).on("click", "a#institucionalMain", function() {
-		$("section#centralSection").load("institucional.html");
-	});
+
 	$(document).on("click", "a#missaoEObjetivo", function() {
+
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("institucional/missao-e-objetivos.html");
+		$("#institucionalMain").removeClass("voltaMenuBranco");
+		$("#institucionalMain").addClass("active");
+		idAnterior = "#institucionalMain";
+
 	});
 	$(document).on("click", "a#conselhoDeliberativo", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("institucional/conselho-deliberativo.html");
+		$("#institucionalMain").removeClass("voltaMenuBranco");
+		$("#institucionalMain").addClass("active");
+		idAnterior = "#institucionalMain";
 	});
+
 	/*exposicao*/
 	$(document).on("click", "a#exposicoes", function() {
-
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("exposicoes/exposicoes.html");
+		$("#exposicoesMain").removeClass("voltaMenuBranco");
+		$("#exposicoesMain").addClass("active");
+		idAnterior = "#exposicoesMain";
 	});
-	$(document).on("click","a#visitas", function()
-	{
+	$(document).on("click", "a#visitas", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("exposicoes/visitas.html");
-	} );
+		$("#exposicoesMain").removeClass("voltaMenuBranco");
+		$("#exposicoesMain").addClass("active");
+		idAnterior = "#exposicoesMain";
+	});
 	/*Educativo*/
-	$(document).on("click","a#videos", function()
-	{
+	$(document).on("click", "a#videos", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("educativo/videos.html");
-	} );
-	
-	/*Doacoes*/	
-	$(document).on("click","a#doacoes", function()
-	{
+		$("#educativoMain").removeClass("voltaMenuBranco");
+		$("#educativoMain").addClass("active");
+		idAnterior = "#educativoMain";
+	});
+
+	/*Doacoes*/
+	$(document).on("click", "a#doacoes", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("doacoes/politica-de-doacoes.html");
-	} );
-	$(document).on("click","a#paraDoar", function()
-	{
+		$("#doacoesMain").removeClass("voltaMenuBranco");
+		$("#doacoesMain").addClass("active");
+		idAnterior = "#doacoesMain";
+	});
+	$(document).on("click", "a#paraDoar", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("doacoes/para-doar.html");
-	} );
+		$("#doacoesMain").removeClass("voltaMenuBranco");
+		$("#doacoesMain").addClass("active");
+		idAnterior = "#doacoesMain";
+	});
 	/*Contatos*/
-	$(document).on("click","a#atendimento", function()
-	{
+	$(document).on("click", "a#atendimento", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("contatos/atendimento.html");
-	} );
-	$(document).on("click","a#localizacaoEHorarios", function()
-	{
+		$("#contatosMain").removeClass("voltaMenuBranco");
+		$("#contatosMain").addClass("active");
+		idAnterior = "#contatosMain";
+	});
+	$(document).on("click", "a#localizacaoEHorarios", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
 		$("section#centralSection").load("contatos/localizacao-e-horarios.html");
-	} );
-	
+		$("#contatosMain").removeClass("voltaMenuBranco");
+		$("#contatosMain").addClass("active");
+		idAnterior = "#contatosMain";
+	});
+
 	/*Acervo*/
-	$(document).on("click","a#acervo", function()
-	{
-		$("section#centralSection").load("acervo.html");
-	} );
-	
+	$(document).on("click", "a#acervoMain", function() {
+		$(idAnterior).addClass("voltaMenuBranco");
+		$("section#centralSection").load("acervo/acervo.html");
+		$("#acervoMain").removeClass("voltaMenuBranco");
+		$("#acervoMain").addClass("active");
+		idAnterior = "#acervoMain";
+	});
+
 });
